5cca1dd9df9f5c6b8fe1b47f9ec22e45adbba053
therealhiddenyeti <72407136+therealhiddenyeti@users.noreply.github.com>
2022-04-11 08:49:10 -0400

    x

--- createFailureLog.py
+++ createFailureLog.py
@@ -1,26 +1,20 @@
  #!/usr/bin/python
-import json
+#import json
+import datetime
 import sys
 import getopt
 import csv
 
 
-def createFailureLog(input_data: str, output_file: str):
-#    csv_columns = ['dimensioner', 'file', 'timestamp']
+def createFailureLog(_dimensioner: str, _erroredFile: str, _epoch_time: str, output_file: str):
+    csv_columns = ['dimensioner', 'file', 'timestamp']
     result_list = []
     with open(input_file) as f:
-        data = json.load(f)
-        for result in data.get('results'):
-            result_list.append({
-                'serialnbr': data.get('serial'),
-                'vdom': data.get('vdom'),
-                'policyid': result.get('policyid'),
-                'srcintf': ','.join([val.get('name') for val in result.get('srcintf')]),
-                'dstintf': ','.join([val.get('name') for val in result.get('dstintf')]),
-                'srcaddr': ','.join([val.get('name') for val in result.get('srcaddr')]),
-                'dstaddr': ','.join([val.get('name') for val in result.get('dstaddr')])
-            }
-            )
+        result_list.append({
+            'dimensioner': _dimensioner,
+            'file': _erroredFile,
+            'timestamp': datetime.datetime.fromtimestamp( _epoch_time ) 
+        })
     try:
         with open(output_file, 'a') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
@@ -35,21 +29,27 @@ def main(argv):
     input_file = ''
     output_file = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "hd:e:t:o:", ["ddimensioner=", "efile=", "ttime=", "ofile="])
     except getopt.GetoptError:
-        print('transform.py -i <inputfile> -o <outputfile>')
+        print('transform.py -d <dimensioner> -e <errorFile> -t <ttime> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
+            print('test.py -d <dimensioner> -e <errorFile> -t <ttime> -o <outputfile>')
             sys.exit()
-        elif opt in ("-i", "--ifile"):
-            input_file = arg
+        elif opt in ("-d", "--ddimensioner"):
+            dimensioner = arg
+        elif opt in ("-e", "--efile"):
+            erroredFile = arg
+        elif opt in ("-t", "--ttime"):
+            epoch_time = arg
         elif opt in ("-o", "--ofile"):
             output_file = arg
-    print('Input file is "', input_file)
+    print('dimensioner is "', dimensioner)
+    print('erroredFile is "', erroredFile)
+    print('epoch_time is "', epoch_time)
     print('Output file is "', output_file)
-    transform(input_file, output_file)
+    createFailureLog(dimensioner, erroredFile, epoch_time, output_file)
 
 
 if __name__ == "__main__":
